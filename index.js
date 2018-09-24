6b869b3af57bbf48d5bb9c2cfc7b98c3d1971b69
Leo Lien <leolien10@gmail.com>
2018-09-24 20:24:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+
+const companyName = 'Scuber';
+
+companyName = 'specialCompany';
+
+let mostProfitableNeighborhood = 'Chelsea';
+
+mostProfitableNeighborhood = 'Upper West Side';
+
+let companyCeo = 'Susan Smith';
+
+companyCeo = 'Lauren Hart';
