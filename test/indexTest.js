4c8b1456df105d1414c6251484e66cf6a95f6fa2
ch2isk4kos <ch2isk4kos@gmail.com>
2019-09-06 16:53:59 -0400

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -40,3 +40,40 @@ describe('index.js', function () {
     });
   });
 });
+
+// NOTES:
+
+// describe
+// a function provided by our test library, Mocha.
+
+// it's used to hold your tests.
+// after the word 'describe' is information about your tests.
+
+// tests are used as a way to document the behavior of a function to developers
+
+// example:
+// the next word 'describe' is followed by the word 'companyName'
+
+// here the test is telling you that the tests that come afterwards
+// will be about companyName.
+
+// then comes the word 'it', where you see the following:
+
+it('is set as Scuber', function () {
+  // tests are here
+});
+
+// this is telling us that the companyName should be set to Scuber.
+// finally, filling in the missing part of the it code, you see:
+
+it('is set as Scuber', function () {
+  expect(companyName).to.equal('Scuber');
+});
+
+// this example shows that the test expects companyName to equal Scuber.
+// the 'expect' and 'to.equal' are essentially doing the same thing as
+// companyName == 'Scuber'
+
+// in other words, expect(companyName).to.equal('Scuber') is running code
+// that will have this first test pass if companyName equals Scuber
+// and fail if it does not.
